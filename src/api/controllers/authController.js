dcb502a1ffece1032de45b0afdd726b16670ef11
Reynaldo Hendson <80369346+reynaldo-hendson@users.noreply.github.com>
2025-06-18 21:10:27 -0300

    Update authController.js

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,6 +1,6 @@
 function login(req, res) {
   const { username, password } = req.body;
-  if (username === '' && password === '') {
+  if (username === 'admin' && password === 'admin') {
     return res.json({ success: true, message: 'Login bem-sucedido' });
   }
   return res.status(401).json({ success: false, message: 'Credenciais inválidas' });
